4562a6946887a91e3220ef6170ac7d8101de62e8
Samy Rahmani <samy.osmium@gmail.com>
2022-12-14 19:49:26 -0500

    Initialized

---
+++ src/main/java/com/samyosm/loremarticulus/LoremArticulusApplication.java
@@ -0,0 +1,13 @@
+package com.samyosm.loremarticulus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoremArticulusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoremArticulusApplication.class, args);
+    }
+
+}
