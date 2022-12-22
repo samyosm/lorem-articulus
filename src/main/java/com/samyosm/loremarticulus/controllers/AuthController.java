cb036b7555f8ba71fc3f9198772aa61c50bdede8
Samy Rahmani <samy.osmium@gmail.com>
2022-12-22 18:15:43 -0500

    Registered user when generated

--- src/main/java/com/samyosm/loremarticulus/controllers/AuthController.java
+++ src/main/java/com/samyosm/loremarticulus/controllers/AuthController.java
@@ -1,12 +1,17 @@
 package com.samyosm.loremarticulus.controllers;
 
+import com.samyosm.loremarticulus.models.UserItem;
 import com.samyosm.loremarticulus.objects.UserRegistration;
+import com.samyosm.loremarticulus.repositories.UserRepo;
 import com.samyosm.loremarticulus.utils.UIDManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -14,6 +19,9 @@ public class AuthController {
 
     @Value("${tokens.admin}")
     private String adminToken;
+
+    @Autowired
+    private UserRepo userRepo;
 
     @PostMapping("/getToken")
     public String Generator(
@@ -24,6 +32,14 @@ public class AuthController {
         if (!authToken.equals(adminToken)) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
         }
-        return UIDManager.getRandomUidToken(hints.uid());
+        var uidToken = UIDManager.getRandomUidToken(hints.uid());
+        UserItem user = new UserItem(
+                hints.uid(),
+                List.of(uidToken),
+                hints.maxUsage(),
+                0
+        );
+        userRepo.save(user);
+        return uidToken;
     }
 }
